b8c367cd303fd109b4c3bfc516d9ce7cc63034fb
fabiensauce <fabiensauce@orange.fr>
2019-01-18 20:46:05 +1100

    autocomplete input unused into folder for the moment

---
+++ src/autocomplete_tmp/Autocomplete.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+
+class Autocomplete extends Component {
+  static defaultProps = {
+    suggestions: []
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // The active selection's index
+      activeSuggestion: 0,
+      // The suggestions that match the user's input
+      filteredSuggestions: [],
+      // Whether or not the suggestion list is shown
+      showSuggestions: false,
+      // What the user has entered
+      userInput: ""
+    };
+  }
+
+  onChange = e => {
+    const { suggestions } = this.props;
+    const userInput = e.currentTarget.value;
+
+    // Filter our suggestions that don't contain the user's input
+    const filteredSuggestions = suggestions.filter(
+      suggestion =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions,
+      showSuggestions: true,
+      userInput: e.currentTarget.value
+    });
+  };
+
+  onClick = e => {
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: e.currentTarget.innerText
+    });
+  };
+
+  onKeyDown = e => {
+    const { activeSuggestion, filteredSuggestions } = this.state;
+    // User pressed the enter key
+    if (e.keyCode === 13) {
+      this.setState({
+        showSuggestions: false,
+        userInput: filteredSuggestions[activeSuggestion]
+      });
+    }
+    // User pressed the up arrow
+    else if (e.keyCode === 38) {
+      if (activeSuggestion === 0) return;
+      this.setState({ activeSuggestion: activeSuggestion - 1 });
+    }
+    // User pressed the down arrow
+    else if (e.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length) return;
+      this.setState({ activeSuggestion: activeSuggestion + 1 });
+    }
+  };
+
+  render() {
+    const {
+      activeSuggestion,
+      filteredSuggestions,
+      showSuggestions,
+      userInput
+    } = this.state;
+
+    let suggestionsListComponent;
+
+    if (showSuggestions && userInput) {
+      if (filteredSuggestions.length) {
+        suggestionsListComponent = (
+          <ul class="suggestions">
+            {filteredSuggestions.map((suggestion, index) => {
+              let className;
+              // Flag the active suggestion with a class
+              if (index === activeSuggestion) {
+                className = "suggestion-active";
+              }
+              return (
+                <li
+                  className={className}
+                  key={suggestion}
+                  onClick={this.onClick}
+                >
+                  {suggestion}
+                </li>
+              );
+            })}
+          </ul>
+        );
+      } else {
+        suggestionsListComponent = (
+          <div class="no-suggestions">
+            {" "}
+            No suggestions, you're on your own!{" "}
+          </div>
+        );
+      }
+    }
+
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={this.onChange}
+          onKeyDown={this.onKeyDown}
+          value={userInput}
+        />
+        {suggestionsListComponent}
+      </div>
+    );
+  }
+}
+
+export default Autocomplete;
